10322c7d7eff33f155786f12d24a2dd31ba6413b
Thierry Moreau <tmoreau@octoml.ai>
2023-08-04 18:36:15 -0400

    update octoshop

--- octoshop.py
+++ octoshop.py
@@ -1,14 +1,24 @@
 import streamlit as st
+# from octoai.client import Client
 from io import BytesIO
 from base64 import b64encode, b64decode
 import requests
 from PIL import Image, ExifTags
 import os
-import cv2
-import numpy as np
+import time
 
 OCTOSHOP_ENDPOINT_URL = os.environ["OCTOSHOP_ENDPOINT_URL"]
 OCTOAI_TOKEN = os.environ["OCTOAI_TOKEN"]
+
+
+def get_request(url):
+    headers = {
+        "Content-type": "application/json",
+        "Authorization": f"Bearer {OCTOAI_TOKEN}",
+    }
+    response = requests.get(url=url, headers=headers)
+    response.raise_for_status()
+    return response.json()
 
 def read_image(image):
     buffer = BytesIO()
@@ -48,18 +58,17 @@ def rescale_image(image):
     return image
 
 def query_octoshop(payload):
-     # Send to SDXL endpoint
+     # Send to SDXL endpoint - async request
     headers = {
         "Content-type": "application/json",
+        "X-OctoAI-Async": "1",
         "Authorization": f"Bearer {OCTOAI_TOKEN}",
     }
     url = f"{OCTOSHOP_ENDPOINT_URL}/generate"
     # Send image for generation with roop
     response = requests.post(url=url, json=payload, headers=headers)
-    # Turn into image
-    image_str = response.json()["image"]
-    image = Image.open(BytesIO(b64decode(image_str)))
-    return image
+    response.raise_for_status()
+    return response.json()
 
 def travel_back(my_upload, meta_prompt):
     colI, colO = st.columns(2)
@@ -70,20 +79,67 @@ def travel_back(my_upload, meta_prompt):
     input_img = rescale_image(input_img)
     colI.write("Input image")
     colI.image(input_img)
+    progress_text = "OctoShopping in action..."
+    percent_complete = 0
+    progress_bar = colO.progress(percent_complete, text=progress_text)
 
-    image_response = query_octoshop({
+    response = query_octoshop({
         "prompt": meta_prompt,
         "strength": 0.80,
         "steps": 20,
         "image": read_image(input_img),
         "faceswap": True
     })
-    colO.write("Transformed image :star2:")
-    colO.image(image_response)
+
+    status = get_request(response["poll_url"])
+    time_step = 0.1
+    time_slept = 0
+    while status["status"] == "pending":
+        time.sleep(time_step)
+        time_slept += time_step
+        # print("Time Slept = {}, status = {}".format(time_slept, status))
+        status = get_request(response["poll_url"])
+        percent_complete = min(99, percent_complete+1)
+        progress_bar.progress(percent_complete, text=progress_text)
+    progress_bar.progress(100, text="Ready!")
+
+    if status["status"] == "completed":
+        results = get_request(status["response_url"])
+        image_str = results["image"]
+        octoshopped_image = Image.open(BytesIO(b64decode(image_str)))
+        progress_bar.empty()
+        colO.write("OctoShopped image :star2:")
+        # Add watermark - TODO - increase robustness
+        watermark = "assets/octoml-octopus-white.png" # watermark image
+        imgS = octoshopped_image.convert("RGBA")
+        imgW = Image.open(watermark)
+        imgW = imgW.resize((200, 200)) # change the numbers to adjust the size
+        imgS.paste(imgW, (0,0), imgW.convert("RGBA"))
+        # Display final image
+        colO.image(imgS)
+    else:
+        colO.write("Oops, something went wrong... OctoShop is in alpha preview, thank you for being patient!")
 
 st.set_page_config(layout="wide", page_title="OctoShop")
 
-st.write("## :tada: OctoShop Preview - Powered by OctoAI")
+# Powered by OctoML displayed in top right
+st.markdown("""
+<style>
+.powered-by {
+    position: absolute;
+    top: -10px;
+    right: 0;
+    float: right;
+}
+.powered-by span {
+    padding-right: 5;
+</style>
+<div class="powered-by">
+<span>Powered by </span> <a href="https://octoai.cloud/"><img src="https://i.ibb.co/T1X1CHG/octoml-octo-ai-logo-vertical-container-white.png" alt="octoml-octo-ai-logo-vertical-container-white" border="0" width="200"></a>
+</div>
+""", unsafe_allow_html=True)
+
+st.write("## :tada: OctoShop Preview")
 
 st.write("### :camera: Transform photos with the power of words and generative AI!")
 
@@ -99,7 +155,7 @@ st.markdown(
     "*Alpha mode engaged*: I can't handle photos with multiple subjects right now! I can handle at most one person in the frame! If you didn't get good results, try again!"
 )
 
-meta_prompt = st.text_input("Transform prompt", value="Set the scene in 80s Tokyo")
+meta_prompt = st.text_input("OctoShop prompt", value="Set the scene in 80s Tokyo")
 
 if my_upload is not None:
     if st.button('OctoShop!'):
